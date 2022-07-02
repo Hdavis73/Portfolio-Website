4c7d16ad4579b322eafe0016faae67354116b200
Heather <lilpinkrose02@gmail.com>
2022-07-02 11:57:37 -0500

    changed flow of page from scrolling to clicking through

--- main.js
+++ main.js
@@ -1,31 +1,71 @@
+//Toggle Navbar
+const navToggler = document.querySelector('.nav-toggler')
+navToggler.addEventListener('click', () => {
+    hideSection()
+    toggleNavbar()
+    document.body.classList.toggle('hide-scrolling')
+})
+function hideSection() {
+    document.querySelector('section.active').classList.toggle('fade-out')
+}
+
+function toggleNavbar() {
+    document.querySelector('.header').classList.toggle('active')
+}
+
+//  ACTIVE SECTION
+document.addEventListener('click', (e) => {
+    if (e.target.classList.contains('link-item') && e.target.hash !== '') {
+
+        navToggler.classList.add('hide')
+        if (e.target.classList.contains('nav-item')) {
+            toggleNavbar()
+        } else {
+            hideSection()
+            document.body.classList.add('hide-scrolling')
+        }
+
+
+        setTimeout(() => {
+            document.querySelector('section.active').classList.remove('active', 'fade-out')
+            document.querySelector(e.target.hash).classList.add('active')
+            window.scrollTo(0, 0)
+            document.body.classList.remove('hide-scrolling')
+            navToggler.classList.remove('hide')
+        }, 200)
+    }
+})
+
+
+
 // ABOUT TABS
 
 const tabsContainer = document.querySelector('.about-tabs')
 let aboutSection = document.querySelector('.about-section')
 
 tabsContainer.addEventListener('click', (e) => {
-   if(e.target.classList.contains('tab-item') && !e.target.classList.contains('active')){
-       tabsContainer.querySelector('.active').classList.remove('active')
-       e.target.classList.add('active')
+    if (e.target.classList.contains('tab-item') && !e.target.classList.contains('active')) {
+        tabsContainer.querySelector('.active').classList.remove('active')
+        e.target.classList.add('active')
 
-       const target = e.target.getAttribute('data-target')
+        const target = e.target.getAttribute('data-target')
 
-       aboutSection.querySelector('.tab-content.active').classList.remove('active')
-       aboutSection.querySelector(target).classList.add('active')
-   }
+        aboutSection.querySelector('.tab-content.active').classList.remove('active')
+        aboutSection.querySelector(target).classList.add('active')
+    }
 })
 
 // -----------PORTFOLIO ITEM DETAILS POPUP------------------------
 
 document.addEventListener('click', (e) => {
-    if(e.target.classList.contains('view-project-btn')){
+    if (e.target.classList.contains('view-project-btn')) {
         togglePortfolioPopup()
-        document.querySelector('.portfolio-popup').scrollTo(0,0)
+        document.querySelector('.portfolio-popup').scrollTo(0, 0)
         portfolioItemDetails(e.target.parentElement)
     }
 })
 
-function togglePortfolioPopup(){
+function togglePortfolioPopup() {
     document.querySelector('.portfolio-popup').classList.toggle('open')
     document.body.classList.toggle('hide-scrolling')
     document.querySelector('.main').classList.toggle('fade-out')
@@ -35,14 +75,17 @@ document.querySelector('.pp-close').addEventListener('click', togglePortfolioPop
 
 //hide popup when clicking outside of it
 document.addEventListener('click', (e) => {
-    if(e.target.classList.contains('pp-inner')){
+    if (e.target.classList.contains('pp-inner')) {
         togglePortfolioPopup()
     }
 })
 
-function portfolioItemDetails(portfolioItem){
+function portfolioItemDetails(portfolioItem) {
     document.querySelector('.pp-thumbnail img').src = portfolioItem.querySelector('.portfolio-item-thumbnail img').src
     document.querySelector('.pp-header h3').innerHTML = portfolioItem.querySelector('.portfolio-item-title').innerHTML
     document.querySelector('.pp-body').innerHTML = portfolioItem.querySelector('.portfolio-item-details').innerHTML
 }
+
+
+
 
